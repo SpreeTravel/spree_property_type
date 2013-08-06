091f46e0cddca286307cb44a983026a4fe685d21
Pedro Quintero Rojas <pqr@uh.cu>
2013-08-06 11:14:21 -0400

    adding property icon

--- app/models/spree/property_decorator.rb
+++ app/models/spree/property_decorator.rb
@@ -2,6 +2,7 @@ module Spree
   Property.class_eval do
     belongs_to :property_type
     attr_accessible :property_type_id
+    has_one :property_icon, class_name: 'Spree::PropertyIcon', :as => :viewable
 
     def property_type_name
       property_type.name rescue 'Properties'
---
+++ app/models/spree/property_icon.rb
@@ -0,0 +1,24 @@
+module Spree
+  class PropertyIcon < Asset
+    validates_attachment_presence :attachment
+    validate :no_attachment_errors
+
+    attr_accessible :alt, :attachment, :position, :viewable_type, :viewable_id
+
+    has_attached_file :attachment,
+                      styles: { mini: '24x24>', small: '48x48>', large: '100x100>'},
+                      default_style: :small,
+                      url: '/spree/icons/properties/:id/:style_:basename.:extension',
+                      path: ':rails_root/public/spree/icons/properties/:id/:style_:basename.:extension',
+                      convert_options: { all: '-strip -auto-orient' }
+
+    def no_attachment_errors
+      unless attachment.errors.empty?
+        # uncomment this to get rid of the less-than-useful interrim messages
+        # errors.clear
+        errors.add :attachment, "Paperclip returned errors for file '#{attachment_file_name}' - check ImageMagick installation or image source file."
+        false
+      end
+    end
+  end
+end
