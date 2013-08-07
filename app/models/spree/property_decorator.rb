bdd91e2be31a798a8f2eaa3321aad758935fa541
Pedro Quintero Rojas <pqr@uh.cu>
2013-08-06 21:53:47 -0400

    ya las properties tienen icono

--- app/models/spree/property_decorator.rb
+++ app/models/spree/property_decorator.rb
@@ -3,11 +3,11 @@ module Spree
     belongs_to :property_type
     attr_accessible :property_type_id, :icon
     has_attached_file :icon,
-                      styles: { mini: '32x32>', normal: '128x128>' },
+                      styles: { mini: '24x24#', small: '48x48#', normal: '128x128#' },
                       default_style: :mini,
                       url: '/spree/taxons/:id/:style/:basename.:extension',
                       path: ':rails_root/public/spree/taxons/:id/:style/:basename.:extension',
-                      default_url: '/assets/default_taxon.png'
+                      default_url: '/assets/default_property.png'
 
     def property_type_name
       property_type.name rescue 'Properties'
