2121eb62dce708dc704c902f4fa81e0eb4bf913b
Pedro Quintero Rojas <pqr@uh.cu>
2013-08-12 21:33:55 -0400

    adicionando un nuevo partial para las properties

--- app/overrides/modify_properties_in_product_show.rb
+++ app/overrides/modify_properties_in_product_show.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
     :virtual_path => 'spree/products/show',
     :name => 'modify_properties_in_product_show',
     :replace => "[data-hook='product_properties']",
-    :partial => "spree/products/enhanced_properties"
+    :partial => "spree/products/enhanced_properties_in_left"
 )
