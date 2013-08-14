ff5197e07024ff971285f9669dd79fb78845c030
Pedro Quintero Rojas <pqr@uh.cu>
2013-08-13 22:01:07 -0400

    updating controller

--- app/controllers/spree/admin/property_types_controller.rb
+++ app/controllers/spree/admin/property_types_controller.rb
@@ -2,7 +2,16 @@ module Spree
   module Admin
     class PropertyTypesController < ResourceController
 
-      #after_filter :do_this,
+      after_filter :set_property_type_show, :only => [:create, :update]
+
+      def set_property_type_show
+        list = []
+        Spree::PropertyType.show_where.each do |k,v|
+          list << k if params["pts_#{k}"]
+        end
+        @property_type.show = list.join('|')
+        @property_type.save
+      end
     end
   end
 end
