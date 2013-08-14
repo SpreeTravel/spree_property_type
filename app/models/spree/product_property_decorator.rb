33ada84edef26a63362f4082bf97df6598b93f4e
Pedro Quintero Rojas <pqr@uh.cu>
2013-08-13 22:40:27 -0400

    bug fixing

--- app/models/spree/product_property_decorator.rb
+++ app/models/spree/product_property_decorator.rb
@@ -1,6 +1,10 @@
 module Spree
   ProductProperty.class_eval do
 
+    # TODO: ahora mismo una property que esté en mas de un grupo
+    # sale en el primero de los grupos, hay que pensar si tiene sentido
+    # o no, que la property salga en todos los grupos
+    # TODO: ademas hay un bug ahi que no he descubierto, que provoca esto
     def self.hash_for(part)
       product_properties_for_this_part = self.joins(:property)
       hash = {}
