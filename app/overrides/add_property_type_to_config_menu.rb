aa8b2dcac15bb91fe58dadc76b0afdc7796fdd17
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2017-05-17 15:34:15 -0400

    udpating to  3.2

--- app/overrides/add_property_type_to_config_menu.rb
+++ app/overrides/add_property_type_to_config_menu.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-    :virtual_path => 'spree/admin/shared/_configuration_menu',
-    :name => 'add_property_type_to_admin_menu',
-    :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-    :text => "<%= configurations_sidebar_menu_item t('property_types'), admin_property_types_path %>"
+    virtual_path: 'spree/admin/shared/sub_menu/_configuration',
+    name: 'add_property_type_to_admin_menu',
+    insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
+    text: "<%= configurations_sidebar_menu_item(Spree.t(:property_types), admin_property_types_path) if can? :manage, Spree::PropertyType %>"
 )
--- spree_property_type.gemspec
+++ spree_property_type.gemspec
@@ -7,7 +7,7 @@ versions = yaml['gems']
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_property_type'
-  s.version     = versions['spree_travel']
+  s.version     = versions['spree']
   s.summary     = 'Types/Categories for properties'
   s.description = 'You can categorize and organize your properties, each type of properties can be located in diferent places in the website'
   s.required_ruby_version = '>= 1.9.3'
